c7308151df0715a6a83a9e009ea251d1b7ed0aea
sumarokov-vp <sumarokov.vp@gmail.com>
2023-10-14 22:19:31 +0600

    disable blankline scope

--- lua/vova/plugins/indent_blankline.lua
+++ lua/vova/plugins/indent_blankline.lua
@@ -1,1 +1,15 @@
-return{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return{
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    require("ibl").setup ({
+      indent = {
+        char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    })
+  end,
+}
